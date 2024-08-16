68c568c32a3b698add70abfdf7645c6eef0eb1cb
uTa4u <nice.baychurin@mail.ru>
2024-08-16 21:44:03 +0300

    Config file

--- src/main/java/su/uTa4u/tfcwoodwork/Config.java
+++ src/main/java/su/uTa4u/tfcwoodwork/Config.java
@@ -12,52 +12,71 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = TFCWoodworking.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    private static final ForgeConfigSpec.IntValue BARK_DROP = BUILDER
+            .comment("How many bark pieces to drop per side")
+            .defineInRange("barkDropCount", 1, 0, 64);
 
-    private static final ForgeConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+    private static final ForgeConfigSpec.IntValue BAST_DROP = BUILDER
+            .comment("How many bast pieces to drop per side")
+            .defineInRange("bastDropCount", 1, 0, 64);
 
-    public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
+    private static final ForgeConfigSpec.IntValue SAWDUST_DROP = BUILDER
+            .comment("How much sawdust should drop from each saw interaction")
+            .defineInRange("sawdustDropCount", 1, 0, 64);
 
-    // a list of strings that are treated as resource locations for items
-    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+    private static final ForgeConfigSpec.IntValue SUPPORT_PER_HALF = BUILDER
+            .comment("How many support should be crafted from one debarked half")
+            .defineInRange("supportPerLogHalf", 4, 0, 64);
+
+    private static final ForgeConfigSpec.IntValue LUMBER_PER_QUARTER = BUILDER
+            .comment("How many lumber should be crafted from one debarked quarter")
+            .defineInRange("lumberPerLogQuarter", 2, 0, 64);
+
+    private static final ForgeConfigSpec.IntValue FENCE_FROM_PLANK = BUILDER
+            .comment("How many fences should drop from each plank interaction")
+            .defineInRange("fenceFromPlank", 2, 0, 64);
+
+    private static final ForgeConfigSpec.IntValue FENCE_FROM_STAIR = BUILDER
+            .comment("How many fences should drop from each stair interaction")
+            .defineInRange("fenceFromStair", 2, 0, 64);
+
+    private static final ForgeConfigSpec.IntValue FENCE_FROM_LOG = BUILDER
+            .comment("How many log fences should drop from each log interaction")
+            .defineInRange("fenceFromLog", 4, 0, 64);
+
+    private static final ForgeConfigSpec.IntValue TRAPDOOR_FROM_SLAB = BUILDER
+            .comment("How many trapdoors should drop from each slab interaction")
+            .defineInRange("trapdoorFromSlab", 2, 0, 64);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
-    public static Set<Item> items;
 
-    private static boolean validateItemName(final Object obj)
-    {
-        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
-    }
+    public static int barkDropCount;
+    public static int bastDropCount;
+    public static int supportPerLogHalf;
+    public static int lumberPerLogQuarter;
+    public static int sawdustDropCount;
+    public static int fenceFromPlank;
+    public static int fenceFromStair;
+    public static int fenceFromLog;
+    public static int trapdoorFromSlab;
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
-                .collect(Collectors.toSet());
+        barkDropCount = BARK_DROP.get();
+        bastDropCount = BAST_DROP.get();
+        supportPerLogHalf = SUPPORT_PER_HALF.get();
+        lumberPerLogQuarter = LUMBER_PER_QUARTER.get();
+        sawdustDropCount = SAWDUST_DROP.get();
+        fenceFromPlank = FENCE_FROM_PLANK.get();
+        fenceFromStair = FENCE_FROM_STAIR.get();
+        fenceFromLog = FENCE_FROM_LOG.get();
+        trapdoorFromSlab = TRAPDOOR_FROM_SLAB.get();
     }
 }
